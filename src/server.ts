bfb16b418e6351792faebab72324faff9bbbbe43
Talha Khan <talha4khan@hotmail.com>
2025-06-04 00:45:01 +0500

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,17 @@
 import { Config } from './config';
+import app from './app';
 console.log(Config.PORT);
+
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    app.listen(PORT, () => {
+      console.log(`${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+startServer();

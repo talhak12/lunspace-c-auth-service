9afa8b8cacc54cc79a4114105497b17ead5e1f01
Talha Khan <talha4khan@hotmail.com>
2025-06-16 13:18:27 +0500

    add initial migration

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -11,9 +11,9 @@ export const AppDataSource = new DataSource({
   username: Config.DB_USERNAME,
   password: Config.DB_PASSWORD,
   database: Config.DB_NAME,
-  synchronize: true, //Config.NODE_ENV == 'test' || Config.NODE_ENV == 'dev',
+  synchronize: false, //Config.NODE_ENV == 'test' || Config.NODE_ENV == 'dev',
   logging: false,
   entities: [User, RefreshToken],
-  migrations: [],
+  migrations: ['src/migration/*.ts'],
   subscribers: [],
 });

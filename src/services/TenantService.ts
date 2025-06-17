d838eed56e329fe7da873199640a700d901c1092
Talha Khan <talha4khan@hotmail.com>
2025-06-17 11:07:50 +0500

    add create tenant endpoint

---
+++ src/services/TenantService.ts
@@ -0,0 +1,11 @@
+import { Tenant } from '../entity/Tenant';
+import { ITenant } from '../types';
+import { Repository } from 'typeorm';
+
+export class TenantService {
+  constructor(private tenantRepository: Repository<Tenant>) {}
+
+  async create(tenantData: ITenant) {
+    return await this.tenantRepository.save(tenantData);
+  }
+}
--- test/users/tenants/create.spec.ts
+++ test/users/tenants/create.spec.ts
@@ -2,6 +2,7 @@ import { DataSource } from 'typeorm';
 import { AppDataSource } from '../../../src/config/data-source';
 import request from 'supertest';
 import app from '../../../src/app';
+import { Tenant } from '../../../src/entity/Tenant';
 
 describe('POST /tenants', () => {
   let connection: DataSource;
@@ -32,6 +33,21 @@ describe('POST /tenants', () => {
       const response = await request(app).post('/tenants').send(tenantData);
 
       expect(response.statusCode).toBe(201);
+    });
+
+    it('should create a tenant in the database', async () => {
+      const tenantData = {
+        name: 'Tenant nameq',
+        address: 'Tenant address',
+      };
+
+      await request(app).post('/tenants').send(tenantData);
+      //Act
+      const tenantRepository = connection.getRepository(Tenant);
+
+      const tenants = await tenantRepository.find();
+      expect(tenants).toHaveLength(1);
+      expect(tenants[0].name).toBe(tenantData.name);
     });
   });
 });
